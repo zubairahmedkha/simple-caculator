899e0622d3ad71b1747d0eda74fe10c2baf2cecc
zubairkhan <zubairahmedniz@gmail.com>
2024-03-16 08:13:08 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+console.clear()
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first numer", type: "number", name: "firstNumber" },
+{message: "Enter second number",type:"number",name:"secondNumber"},
+{message: "select one of the operators",type:"list",
+name:"operator",
+choices:["addition",
+"substraction","divion","multiplication"],
+},
+]);
+// conditional statement
+if (answer.operator ==="addition") {
+console.log("your value is:", answer.firstNumber 
++ answer.secondNumber);
+}else if (answer.operator ==="substraction") {
+    console.log("your value is:", answer.firstNumber 
+    - answer.secondNumber);}
+    else if (answer.operator ==="divion") {
+        console.log("your value is:", answer.firstNumber 
+        / answer.secondNumber);}
+        else if (answer.operator ==="multiplication") {
+            console.log("your value is:", answer.firstNumber 
+            * answer.secondNumber);}
+            else {
+ console.log("please select valid value")}   
+ console.log ("zubair khan ");
+
